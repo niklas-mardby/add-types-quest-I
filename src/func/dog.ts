adc10660fdff953f7f19a3ab5217eaac17885ccf
Niklas Mårdby <niklas.mardby@gmail.com>
2024-12-09 11:55:43 +0100

    Finished commonColor

--- src/func/dog.ts
+++ src/func/dog.ts
@@ -11,14 +11,14 @@ export const numberOfColors = (dogs: Dog[]): number => {
 	const uniqueColors: string[] = [];
 
 	dogs.forEach((dog) => {
-		console.log(dog.color);
+		//console.log(dog.color);
 
 		// kolla om dog.color finns i uniqueColors
 		// om inte så lägg till
 		// annars gå vidare
 		const thisColor = dog.color;
 		const index = uniqueColors.indexOf(thisColor);
-		console.log(index);
+		// console.log(index);
 		if (index === -1) uniqueColors.push(dog.color);
 		//if (!colorExists) uniqueColors.push(dog.color);
 	});
@@ -26,4 +26,28 @@ export const numberOfColors = (dogs: Dog[]): number => {
 	return uniqueColors.length;
 };
 
-export const commonColor = (d) => {};
+export const commonColor = (dogs: Dog[]): string => {
+	// Den vanligaste färgen bland alla hundar är: ${commonColor(dogs)}
+
+	const colors: string[] = [];
+	const amounts: number[] = [];
+
+	dogs.forEach((dog) => {
+		// titta på dess färg
+		// har vi sett den färgen innan?
+		// hur många ggr då?
+
+		const index = colors.indexOf(dog.color);
+		//console.log(index);
+		if (index === -1) {
+			colors.push(dog.color);
+			amounts.push(1);
+		} else {
+			amounts[index]++;
+		}
+	});
+	// console.log(colors, amounts);
+
+	const maxAmount = Math.max(...amounts);
+	return colors[amounts.indexOf(maxAmount)];
+};
